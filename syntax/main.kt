6d3c6eb32044a459369e52a05d78a799718648af
wambug <wambugumacharia35@gmail.com>
2022-10-24 08:39:55 +0300

    what netxt?

--- syntax/main.kt
+++ syntax/main.kt
@@ -1,88 +1,94 @@
 // Calculating the rsa alglorithm
-class Rsa (primep:Int,primeq:Int) {
-var p:Int
-var q:Int
-var pq:Int
-var n:Int
-init {
-  p = primep
-  q = primeq
-  pq = p * q
-  n = (p-1) * (q-1)
+class Rsa(primep: Int, primeq: Int) {
+    var p: Int
+    var q: Int
+    var pq: Int
+    var n: Int
+    init {
+        p = primep
+        q = primeq
+        pq = p * q
+        n = (p - 1) * (q - 1)
+    }
+
+    // input should be where there user wants range of prime number to be selected from
+    fun calculate_e(x: Int): Int {
+        var flag = false
+        if (x < 2) {
+            throw Exception("invalid number calculate_e $x")
+        } else {
+            // we are going to chek if a no. is prime according to range given
+            // we append the prime values to an array
+            // check the gcd of the prime values
+            // we also append the prime values with a gcd of one to an array
+            // pick the lowest value with gcd of one from the array to reduce working with large values
+            // we've gotten e
+            var gcd = gcd(x, n)
+            if (gcd == 1) {
+                return x
+            } else {
+                return checkPrime((x * x) + 2)
+            }
+        }
+        return 0
+    }
+
+    // used to calculate private key
+    fun calculate_d(e: Int): Int {
+        // formula for calculating d = ((n*i) +1)/e
+        // i is unknown and will help us get d
+        // d should have a modulus of 0
+        var number = 0
+        var d = 0
+        while (number < 1) {
+            number++
+            var wegotd = ((n * number) + 1) / e
+            if (wegotd % e == 0) {
+                d = wegotd
+                break
+            } else {
+                calculate_d(e + 1)
+            }
+        }
+        return d
+    }
 }
-  // input should be where there user wants range of prime number to be selected from
-  fun calculate_e(x:Int):Int {
+
+fun gcd(x: Int, y: Int): Int {
+    var e = x
+    if (y == 0) {
+        return x
+    } else {
+        return gcd(y, x % y)
+    }
+}
+
+fun checkPrime(num: Int): Int {
     var flag = false
-    if (x < 2){
-        throw Exception ("invalid number calculate_e $x")
-      }else{
-        var gcd = gcd(x,n)
-        if( gcd == 1) {
-           return x
-          } else {
-            return checkPrime((x*x)+2)
-          }
-    }  
-    return 0
-  }
-
-  // used to calculate private key 
-  fun calculate_d(e:Int):Int {
-   // formula for calculating d = ((n*i) +1)/e
-   // i is unknown and will help us get d
-   // d should have a modulus of 0
-  var number = 0
-  var d = 0
-  while (number < 1) {
-  number ++
-  var wegotd = ((n*number)+1)/e  
-  if (wegotd % e == 0){
-    d = wegotd
-    break
-    } else{
-        calculate_d(e+1)
-      }
-  }
-  return d
-  }
-}
-
-fun gcd(x:Int,y:Int):Int {
-  var e = x
-  if (y == 0){
-      return x
-    }else{
-      return gcd(y,x%y)
-  }
-}
-
-fun checkPrime(num:Int):Int { 
-    var flag = false
-    var rsa = Rsa(11,13)
+    var rsa = Rsa(11, 13)
     for (i in 2..num / 2) {
         // condition for nonprime number
         if (num % i == 0) {
             flag = true
             break
-        } else{
-                var e = rsa.calculate_e(num)
-        if (e > 1 && e < ((11-1) * (13-1))) {
-          return e
+        } else {
+            var e = rsa.calculate_e(num)
+            if (e > 1 && e < ((11 - 1) * (13 - 1))) {
+                return e
+            }
         }
-      }
     }
     return rsa.calculate_e(29)
 }
 
 fun main() {
-  fun double_var(arg : Double) {
-    println(arg)
-  }
-    for (i in 2..20){
-     var e = checkPrime(i)
-     println(e)
+    fun double_var(arg: Double) {
+        println(arg)
     }
-  var x = 42.2
-  double_var(x)
+    for (i in 2..20) {
+        var e = checkPrime(i)
+        println(e)
+    }
+    var x = 42.2
+    double_var(x)
 }
-
